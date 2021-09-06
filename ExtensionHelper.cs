76063b2f2f0a6cc3ddacaaf1929131f55f6a9c87
fargeartech87 <farisgear@gmail.com>
2021-09-06 12:14:35 +0800

    initial commit

---
+++ ExtensionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace EnumDesc
+{
+    public static class ExtensionHelper
+    {
+        public static string GetDescription<T>(this T e) where T : IConvertible
+        {
+            if (e is Enum)
+            {
+                Type type = e.GetType();
+                foreach (int val in Enum.GetValues(type))
+                {
+                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memInfo = type.GetMember(type.GetEnumName(val));
+                        DescriptionAttribute descriptionAttribute = memInfo[0]
+                            .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                            .FirstOrDefault() as DescriptionAttribute;
+                        if (descriptionAttribute != null)
+                        {
+                            return descriptionAttribute.Description;
+                        }
+                    }
+                }
+            }
+
+            return null; // could also return string.Empty
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EnumDesc
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = UserType.Staff.GetDescription();
+            Console.WriteLine(item);
+        }
+    }
+
+    public enum UserType
+    {
+        [Description("Student info")]
+        Student,
+        [Description("Staff info")]
+        Staff
+    }
+}
